6e43b1ca2cb24edca5597ad75168304592bfff76
Andrii55 <117086425+Andrii55@users.noreply.github.com>
2023-03-23 14:17:15 +0200

    First-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+
+import * as basicLightbox from "basiclightbox";
+import "basiclightbox/dist/basicLightbox.min.css";
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `)
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryRef.addEventListener("click", onClickImg);
+
+function onClickImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modal = basicLightbox.create(`<img src="${e.target.dataset.source}" width="800" height="600">`);
+  modal.show();
+}
